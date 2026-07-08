893561976b901cb262e9bbfcbbf0d81881c534d0
rebeccavicente <rvallezicunhaesilva@byupathway.edu>
2026-07-08 07:21:18 -0300

    yoooooooooooo

---
+++ scripts/temples.js
@@ -0,0 +1,28 @@
+// Footer copyright year
+document.getElementById("currentyear").textContent =
+new Date().getFullYear();
+
+
+// Footer last modified date
+document.getElementById("lastModified").textContent =
+`Last Modification: ${document.lastModified}`;
+
+
+// Hamburger menu
+const menuButton = document.getElementById("menu");
+const navMenu = document.getElementById("nav-menu");
+
+
+menuButton.addEventListener("click", () => {
+
+    navMenu.classList.toggle("show");
+
+    if (navMenu.classList.contains("show")) {
+        menuButton.textContent = "❌";
+        menuButton.setAttribute("aria-label", "Close Menu");
+    } else {
+        menuButton.textContent = "☰";
+        menuButton.setAttribute("aria-label", "Open Menu");
+    }
+
+});
